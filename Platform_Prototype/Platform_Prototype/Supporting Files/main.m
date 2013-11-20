4b8ea0a80dcd6f5ec23705b6978bb2a997c8cc46
Joshua <jrscorca@gmail.com>
2013-11-19 16:43:14 -0800

    +recommit new project

---
+++ Platform_Prototype/Platform_Prototype/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Platform_Prototype
+//
+//  Created by Joshua Scorca on 11/19/13.
+//  Copyright Joshua Scorca 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
